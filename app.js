74960378cd557b111b24bdf0ddad19c72e0f5c90
Pranshu1902 <pranshu1902@gmail.com>
2022-12-07 23:27:25 +0530

    completed admin authentication

--- app.js
+++ app.js
@@ -2,32 +2,158 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const admin = require("./models/admin");
+const { Admin } = require("./models");
+const bcrypt = require("bcrypt");
+var cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const connectEnsureLogin = require("connect-ensure-login");
+const session = require("express-session");
+const localStrategy = require("passport-local");
+const passport = require("passport");
+const flash = require("connect-flash");
+
+const saltRounds = 10;
+
+app.use(flash());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser("ssh! some secret string!"));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (request, response) => {
-  response.send("hello world");
+app.use(
+  session({
+    secret: "my-super-secret-key-2178172615261562",
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
+
+app.use(function (request, response, next) {
+  response.locals.messages = request.flash();
+  next();
 });
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new localStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    (username, password, done) => {
+      Admin.findOne({ where: { email: username } })
+        .then(async (user) => {
+          const result = await bcrypt.compare(password, user.password);
+          if (result) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: "Invalid password" });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          return done(null, false, {
+            message: "This email is not registered",
+          });
+        });
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  console.log("Serializing user in session", user.id);
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  Admin.findByPk(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((error) => {
+      done(error, null);
+    });
+});
+
+// home page
+app.get("/", (request, response) => {
+  response.render("home");
+});
+
+// signup page frontend
+app.get("/signup", (request, response) => {
+  response.render("signup");
+});
+
+// login page frontend
+app.get("/login", (request, response) => {
+  response.render("login");
+});
+
+// admin home page frontend
+app.get(
+  "/home",
+  connectEnsureLogin.ensureLoggedIn(),
+  async (request, response) => {
+    const loggedInAdminID = request.user.id;
+
+    const admin = await Admin.findByPk(loggedInAdminID);
+    response.render("adminHome", { username: admin.name });
+  }
+);
 
 // create new admin user
-app.post("/signup", async (request, response) => {
-  const res = await admin.createAdmin(
-    request.body.name,
-    request.body.email,
-    request.body.password
-  );
-  return response.send(res);
+app.post("/users", async (request, response) => {
+  // hasing the password
+  const hashpwd = await bcrypt.hash(request.body.password, saltRounds); // take time so add await
+  try {
+    const user = await Admin.create({
+      name: request.body.name,
+      email: request.body.email,
+      password: hashpwd,
+    });
+    request.login(user, (err) => {
+      if (err) {
+        console.log(err);
+        response.redirect("/");
+      } else {
+        request.flash("success", "Sign up successful");
+        response.redirect("/home");
+      }
+    });
+  } catch (error) {
+    request.flash("error", error.message);
+    return response.redirect("/signup");
+  }
 });
 
-app.get("/signup", (request, response) => {
-  return response.render("signup");
+// signout admin
+app.get("/signout", (request, response) => {
+  request.logout((err) => {
+    if (err) {
+      return next(err);
+    } else {
+      response.redirect("/");
+    }
+  });
 });
 
-app.get("/login", (request, response) => {
-  return response.render("login");
-});
+app.post(
+  "/session",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  function (request, response) {
+    console.log(request.user);
+    response.redirect("/home");
+  }
+);
 
 module.exports = app;
