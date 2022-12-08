d433c9bf6f367a164ea166646c5f23ed9e25b75b
Pranshu1902 <pranshu1902@gmail.com>
2022-12-08 14:36:38 +0530

    added admin tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,6 +1,58 @@
 /* eslint-disable no-undef */
+const request = require("supertest");
+
+const db = require("../models/index");
+const app = require("../app");
+
+let server, agent;
+
+const login = async () => {
+  await agent
+    .post("/session")
+    .send({ name: "test@user.com", password: "12345678" });
+};
+
 describe("first", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+    server = app.listen(3000, () => {});
+    agent = request.agent(server);
+  });
+
+  afterAll(async () => {
+    await db.sequelize.close();
+    server.close();
+  });
+
   test("should first", () => {
     expect(1).toBe(1);
+  });
+
+  test("login required", async () => {
+    const res = await agent.get("/home");
+    expect(res.statusCode).toBe(302);
+  });
+
+  test("admin authentication page works", async () => {
+    const res = await agent.get("/signup");
+    expect(res.statusCode).toBe(200);
+    const login = await agent.get("/login");
+    expect(login.statusCode).toBe(200);
+  });
+
+  test("signup admin", async () => {
+    const res = await agent.post("/users").send({
+      name: "admin",
+      email: "test@user.com",
+      password: "12345678",
+    });
+    expect(res.statusCode).toBe(302);
+  });
+
+  test("signout admin", async () => {
+    login();
+    await agent.get("/signout");
+    const res = await agent.get("/home");
+    expect(res.statusCode).toBe(302);
   });
 });
