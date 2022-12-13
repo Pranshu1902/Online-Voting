92a95d5bd24536f039b452893a8e7c0b4ef8de18
Pranshu Aggarwal <pranshu1902@gmail.com>
2022-12-13 23:45:32 +0530

    fixed failing tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,10 +1,16 @@
 /* eslint-disable no-undef */
 const request = require("supertest");
+const cheerio = require("cheerio");
 
 const db = require("../models/index");
 const app = require("../app");
 
 let server, agent;
+
+function extractCsrfToken(res) {
+  var $ = cheerio.load(res.text);
+  return $("[name=_csrf]").val();
+}
 
 const login = async () => {
   await agent
@@ -40,57 +46,74 @@ describe("first", () => {
     expect(login.statusCode).toBe(200);
   });
 
-  // test("signup admin", async () => {
-  //   const res = await agent.post("/users").send({
-  //     name: "admin",
-  //     email: "test@user.com",
-  //     password: "12345678",
-  //   });
-  //   expect(res.statusCode).toBe(302);
-  // });
+  test("signup admin", async () => {
+    const signupPage = await agent.get("/signup");
+    const token = extractCsrfToken(signupPage);
+    const res = await agent.post("/users").send({
+      name: "admin",
+      email: "test@user.com",
+      password: "12345678",
+      _csrf: token,
+    });
+    expect(res.statusCode).toBe(302);
+  });
 
-  // test("create election", async () => {
-  //   let count, newCount;
-  //   const response = await agent.get("/election");
-  //   count = response.body.elections.length;
+  test("create election", async () => {
+    let count, newCount;
+    const addElection = await agent.get("/elections/new");
+    const token = extractCsrfToken(addElection);
+    const response = await agent.get("/election");
+    count = response.body.elections.length;
 
-  //   await agent.post("/election").send({
-  //     name: "test election",
-  //   });
+    await agent.post("/election").send({
+      name: "test election",
+      _csrf: token,
+    });
 
-  //   await agent.get("/election").then((data) => {
-  //     newCount = data.body.elections.length;
-  //   });
-  //   expect(newCount).toBe(count + 1);
-  // });
+    await agent.get("/election").then((data) => {
+      newCount = data.body.elections.length;
+    });
+    expect(newCount).toBe(count + 1);
+  });
 
-  // test("delete election", async () => {
-  //   let count;
-  //   const response = await agent.get("/election");
-  //   count = response.body.elections.length;
+  test("delete election", async () => {
+    let count;
+    const response = await agent.get("/election");
+    count = response.body.elections.length;
 
-  //   const electionID = response.body.elections[count - 1].id;
+    const electionID = response.body.elections[count - 1].id;
 
-  //   const res = await agent.delete(`/election/${electionID}`);
+    const electionPage = await agent.get(`/election/${electionID}`);
+    const token = extractCsrfToken(electionPage);
+    const res = await agent.delete(`/election/${electionID}`).send({
+      _csrf: token,
+    });
 
-  //   expect(res.statusCode).toBe(200);
-  // });
+    expect(res.ok).toBe(true);
+  });
 
-  // test("edit election", async () => {
-  //   await agent.post("/election").send({
-  //     name: "update election",
-  //   });
-  //   let count;
-  //   let response = await agent.get("/election");
-  //   count = response.body.elections.length;
+  test("edit election", async () => {
+    const res = await agent.get("/elections/new");
+    const token = extractCsrfToken(res);
+    await agent.post("/election").send({
+      name: "update election",
+      _csrf: token,
+    });
+    let count;
+    let response = await agent.get("/election");
+    count = response.body.elections.length;
 
-  //   const electionID = response.body.elections[count - 1].id;
+    const electionID = response.body.elections[count - 1].id;
 
-  //   await agent.post(`/election/${electionID}`).send({ name: "New Name" });
+    const electionPage = await agent.get(`/election/${electionID}`);
+    const newToken = extractCsrfToken(electionPage);
+    await agent
+      .post(`/election/${electionID}`)
+      .send({ name: "New Name", _csrf: newToken });
 
-  //   response = await agent.get("/election");
-  //   expect(response.body.elections[count - 1].name).toBe("New Name");
-  // });
+    response = await agent.get("/election");
+    expect(response.body.elections[count - 1].name).toBe("New Name");
+  });
 
   test("signout admin", async () => {
     login();
